84644c60397f20341fbf020cbbf1ab3ebc657e03
NorthboundPaddler <48971077+NorthboundPaddler@users.noreply.github.com>
2025-06-23 21:57:27 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,6 +8,23 @@
       system = "x86_64-linux";
     in
     {
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          packages = with pkgs;  [
+            cargo
+            rustc
+            pkg-config
+            openssl
+          ];
+          shellHook = ''
+            echo "Developing with Rust!"
+          '';
+        };
       packages.${system}.default =
         let
           pkgs = import nixpkgs {
